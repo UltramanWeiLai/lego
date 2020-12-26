2d116482b3072b8f7d31b5e587c23b0c751c1006
Viking Zhang <vikingmute@gmail.com>
2020-12-26 20:21:51 +0800

    add basic vuex store to the whole app

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue'
 import router from './routes/index'
+import store from './store/index'
 import 'ant-design-vue/dist/antd.css'
 const app = createApp(App)
-app.use(Antd).use(router)
+app.use(Antd).use(router).use(store)
 app.mount('#app')
