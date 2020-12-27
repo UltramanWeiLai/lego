29bede4be47505262d83948314389002c4acb816
Viking Zhang <vikingmute@gmail.com>
2020-12-27 10:48:52 +0800

    add mutations and test getters for the vuex store

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -5,6 +5,7 @@
         <div class="page-title">
           <router-link to="/">慕课乐高</router-link>
         </div>
+        <user-profile :user="user"></user-profile>
       </a-layout-header>
       <a-layout-content class="home-layout">
         <router-view></router-view>
@@ -17,13 +18,31 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
+import { useStore } from 'vuex'
+import { GlobalDataProps } from '../store/index'
+import UserProfile from '../components/UserProfile.vue'
 export default defineComponent({
   name: 'Index',
+  components: {
+    UserProfile
+  },
+  setup () {
+    const store = useStore<GlobalDataProps>()
+    const user = computed(() => store.state.user)
+    return {
+      user
+    }
+  }
 })
 </script>
 
 <style>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 .page-title {
   color: #fff;
 }
